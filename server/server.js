509f04f2951e83bb476495016e404f6227a793a0
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2023-01-08 19:33:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,4 +36,4 @@ app.post('/', async (req, res) => {
       res.status(500).send(error || 'Something went wrong');
     }
   })
-  app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+  app.listen(4001, () => console.log('AI server started on http://localhost:4001'))
